a84dcdcd17fa135a062ec09b064ebb7b8db3456b
aziza_dx <nasrallahh55@gmail.com>
2019-09-27 23:59:30 +0300

    add nodemon package and add two get methods

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const app = express();
 
@@ -9,9 +10,33 @@ const listMovieTypes =
     {id:3,movieType:'Horror'}
 ];
 
-app.get('/', (req,res) =>{
-    res.send([listMovieTypes]);
+//This could be our page to see list of all movietypes
+//And the get method is use to Read(display)content 
+app.get('/',(req,res)=>{
+    res.send('Welcome to Movie Rent Api :)))))');
 });
+
+app.get('/api/listmovietypes', (req,res) =>{
+    res.send([listMovieTypes]);//return an array of listmovietype for now
+});
+
+//Getting a singl movietype
+app.get('/api/listmovietypes/:id',(req,res)=>{
+    //get what the user selected. step one
+    //lookup the movieType. step two
+    //if not found , return error. step three
+    //Else return the movietype object. setp four
+    //--------------------------------------------//
+    //step one
+    const movieType = listMovieTypes.find(c => c.id === parseInt(req.params.id));//this will help us to get what the user is selected
+    console.log(movieType);
+    //step two & step three
+    if(!movieType)//which mean didsn't exist
+    return res.status(404).send('Movie Type is not found :(((((');
+    //step four
+    res.send(movieType);
+});
+
 
 app.listen(5000);
 console.log('listening to port 5000');
